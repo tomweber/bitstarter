0735542f84867eb2e86ba03f3a12ca27c8192492
Thomas Weber <thomas@tomweber.eu>
2013-07-16 16:20:07 +0200

    web.js: Read from file

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
+
+var file = fs.openSync("./index.html", 'r');
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
